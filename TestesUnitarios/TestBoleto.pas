2f146f837a7227b7d65f136b5dac55905d5c71e9
Luiz Carlos <luiz_sistemas@hotmail.com>
2019-05-23 20:49:54 -0300

    implementando banco bradesco

--- TestesUnitarios/TestBoleto.pas
+++ TestesUnitarios/TestBoleto.pas
@@ -18,15 +18,7 @@ type
       procedure TestNumeroDoBancoComEspacos;
       procedure TestDigitoDoNumeroDoBancoComEspacos;
       procedure TestNumeroDoBancoEhValido;
-      procedure TestDigitoDoNumeroDoBancoInvalido;
-      procedure TestFormatacaoDoNumeroDaAgencia;
-      procedure TestFormatacaoDoNumeroDaConta;
       procedure TestNumeroDaContaComCarectereInvalido;
-      procedure TestFormatacaoNossoNumeroConvenioDeTamanho4;
-      procedure TestFormatacaoNossoNumeroConvenioDeTamanho6;
-      procedure TestFormatacaoNossoNumeroConvenioDeTamanho7;
-      procedure TestFormatacaoCampoLivreParaConvenio4E6;
-      procedure TestFormatacaoCampoLivreParaConvenio7;
   end;
 
 implementation
@@ -85,73 +77,6 @@ begin
   CheckEquals('9', FBoleto.GetConta.Banco.DigitoNumero , 'Dígito do Número do banco inválido!');
 end;
 
-procedure TestTBoleto.TestDigitoDoNumeroDoBancoInvalido;
-begin
-  StartExpectingException(exception);
-  FBoleto.GetConta.Banco.DigitoNumero := '8';
-  FBoleto.Gerar;
-  StopExpectingException('Nenhuma exceção gerada quando informado dígito do número do banco inválido');
-end;
-
-procedure TestTBoleto.TestFormatacaoCampoLivreParaConvenio4E6;
-begin
-  FBoleto.GetConta.SeqNossoNumero := 123;
-  FBoleto.GetConta.Convenio := '1234';
-  FBoleto.GetConta.Agencia := '123';
-  FBoleto.GetConta.Conta := '1234';
-  FBoleto.GetConta.Carteira := '17';
-  FBoleto.Gerar;
-  CheckEquals('1234000012301230000123417', FBoleto.GetCampoLivre, 'Formatação do Campo Livre Inválido para convênio com 4 e 6 dígitos');
-end;
-
-procedure TestTBoleto.TestFormatacaoCampoLivreParaConvenio7;
-begin
-  FBoleto.GetConta.SeqNossoNumero := 123;
-  FBoleto.GetConta.Convenio := '1234567';
-  FBoleto.GetConta.Agencia := '123';
-  FBoleto.GetConta.Conta := '1234';
-  FBoleto.GetConta.Carteira := '17';
-  FBoleto.Gerar;
-  CheckEquals('0000001234567000000012317', FBoleto.GetCampoLivre, 'Formatação do Campo Livre Inválido para convênio com 7 dígitos');
-end;
-
-procedure TestTBoleto.TestFormatacaoDoNumeroDaAgencia;
-begin
-  FBoleto.GetConta.Agencia := '123';
-  FBoleto.Gerar;
-  CheckEquals('0123', FBoleto.GetConta.Agencia, 'Agência com formato inválido!');
-end;
-
-procedure TestTBoleto.TestFormatacaoDoNumeroDaConta;
-begin
-  FBoleto.GetConta.Conta := '12345';
-  FBoleto.Gerar;
-  CheckEquals('00012345', FBoleto.GetConta.Conta, 'Formatação do número da conta inválido');
-end;
-
-procedure TestTBoleto.TestFormatacaoNossoNumeroConvenioDeTamanho4;
-begin
-  FBoleto.GetConta.SeqNossoNumero := 123;
-  FBoleto.GetConta.Convenio := '1234';
-  FBoleto.Gerar;
-  CheckEquals('12340000123', FBoleto.GetTitulo.NossoNum, 'Formatação Nosso Número inválido para convênio com 4 dígitos');
-end;
-
-procedure TestTBoleto.TestFormatacaoNossoNumeroConvenioDeTamanho6;
-begin
-  FBoleto.GetConta.SeqNossoNumero := 123;
-  FBoleto.GetConta.Convenio := '123456';
-  FBoleto.Gerar;
-  CheckEquals('12345600123', FBoleto.GetTitulo.NossoNum, 'Formatação Nosso Número inválido para convênio com 4 dígitos');
-end;
-
-procedure TestTBoleto.TestFormatacaoNossoNumeroConvenioDeTamanho7;
-begin
-  FBoleto.GetConta.SeqNossoNumero := 123;
-  FBoleto.GetConta.Convenio := '1234567';
-  FBoleto.Gerar;
-  CheckEquals('12345670000000123', FBoleto.GetTitulo.NossoNum, 'Formatação Nosso Número inválido para convênio com 4 dígitos');
-end;
 
 procedure TestTBoleto.TestNumeroDaContaComCarectereInvalido;
 begin
